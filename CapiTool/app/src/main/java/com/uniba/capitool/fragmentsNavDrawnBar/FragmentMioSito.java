ff9a634f17750dfcdd808c01419e2968d464843b
Raffaele <raffaeleciscutti@gmail.com>
2021-12-10 12:31:09 +0100

    Creazione AggiungiSito non ancora DEFINITIVA

--- CapiTool/app/src/main/java/com/uniba/capitool/fragmentsNavDrawnBar/FragmentMioSito.java
+++ CapiTool/app/src/main/java/com/uniba/capitool/fragmentsNavDrawnBar/FragmentMioSito.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.uniba.capitool.R;
 
@@ -20,10 +21,24 @@ public class FragmentMioSito extends Fragment {
     }
 
 
-    @Override
+   @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mio_sito, container, false);
-    }
+
+       View v = inflater.inflate(R.layout.fragment_mio_sito, container, false);
+       Button addSito = v.findViewById(R.id.buttonAddSito);
+
+       addSito.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+
+           }
+       });
+
+       return v;
+
+   }
+
+
 }
