173ed1295f5dc0c8b6f87586cec935c72309dfb9
Raffaele <raffaeleciscutti@gmail.com>
2021-12-11 13:51:59 +0100

    Implementazione Aggiungi Sito del button indietro e salvataggio dati passando da un fragment ad un altro

--- CapiTool/app/src/main/java/com/uniba/capitool/fragments/fragmentsAggiungiInfoSito/AggiungiNomeSito.java
+++ CapiTool/app/src/main/java/com/uniba/capitool/fragments/fragmentsAggiungiInfoSito/AggiungiNomeSito.java
@@ -1,15 +1,19 @@
 package com.uniba.capitool.fragments.fragmentsAggiungiInfoSito;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.uniba.capitool.R;
 
@@ -21,6 +25,17 @@ public class AggiungiNomeSito extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_aggiungi_nome_sito, container, false);
         Button avanti = v.findViewById(R.id.button);
+        EditText nome =v.findViewById(R.id.nomeSito);
+        //leggere il file SharedPreferences
+        SharedPreferences sharedPreferences = getActivity().getPreferences(Context.MODE_PRIVATE);
+        if(sharedPreferences!=null){
+            String nomeTrovato = sharedPreferences.getString("nome", "");
+            nome.setText(nomeTrovato);
+
+        }else{
+            Log.e( "onCreateView: ", "SharedPreferences non trovato");
+        }
+
         avanti.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -28,6 +43,12 @@ public class AggiungiNomeSito extends Fragment {
                 FragmentTransaction fragmentTransaction= fragmentManager.beginTransaction();
                 fragmentTransaction.replace(R.id.fragmentContainerAggiungiSito, new AggiungiInfoSito());
                 fragmentTransaction.commit();
+
+                //scrivere nel file SharedPreferences
+                SharedPreferences sharedPreferences = getActivity().getPreferences(Context.MODE_PRIVATE);
+                SharedPreferences.Editor datiSito = sharedPreferences.edit();
+                datiSito.putString("nome", nome.getText().toString());
+                datiSito.apply();
             }
         });
 
