468a30b2d82ae42aefbce8bc08b59cc98da15789
Marco <marcoa7@outlook.it>
2021-12-22 20:46:53 +0100

    Modificati i layout, implementati controlli su sito tramite query e toast per le funzionalità del curatore in caso di sito non associato

--- CapiTool/app/src/main/java/com/uniba/capitool/fragments/fragmentsNavDrawnBar/FragmentImpostazioniSito.java
+++ CapiTool/app/src/main/java/com/uniba/capitool/fragments/fragmentsNavDrawnBar/FragmentImpostazioniSito.java
@@ -2,26 +2,44 @@ package com.uniba.capitool.fragments.fragmentsNavDrawnBar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.uniba.capitool.R;
 import com.uniba.capitool.activities.BasicMethod;
 import com.uniba.capitool.activities.DelegaSito;
 import com.uniba.capitool.activities.EliminaSito;
 import com.uniba.capitool.classes.Curatore;
+import com.uniba.capitool.classes.SitoCulturale;
 import com.uniba.capitool.classes.Visitatore;
+import com.uniba.capitool.fragments.fragmentsMioSito.FragmentInfoSito;
+
+import java.util.ArrayList;
 
 
 /**
  * create an instance of this fragment.
  */
 public class FragmentImpostazioniSito extends Fragment {
+
+    SitoCulturale sito = null ;
+    DatabaseReference myRef ;
 
 
     public FragmentImpostazioniSito() {
@@ -39,33 +57,57 @@ public class FragmentImpostazioniSito extends Fragment {
         TextView delega_sito = (TextView) v.findViewById(R.id.text_delega_sito) ;
         TextView elimina_sito = v.findViewById(R.id.text_elimina_sito) ;
 
-        delega_sito.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intentDelegaSito = new Intent(getActivity(), DelegaSito.class) ;
-                intentDelegaSito.putExtra("uid", utente.getUid()); //Optional parameters
-                intentDelegaSito.putExtra("nome", utente.getNome()); //Optional parameters
-                intentDelegaSito.putExtra("cognome", utente.getCognome()); //Optional parameters
-                intentDelegaSito.putExtra("email", utente.getEmail()); //Optional parameters
-                intentDelegaSito.putExtra("ruolo", utente.getRuolo()); //Optional parameters
-                getActivity().startActivity(intentDelegaSito);
-            }
-        });
+
+            delega_sito.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    checkSitoAssociatoAlCuratore(utente.getUid());
+                    Log.e("controlloSito", "Ho controllato se è associato il sito");
+
+                    if(sito == null){
+                    Toast.makeText(getActivity().getApplicationContext(), "Operazione non eseguibile: non è associato alcun sito", Toast.LENGTH_SHORT).show();
+                    Log.e("toat_creare", "dovrebbe essere visuaizzato il toast") ;
+                    }else {
+
+                    Log.e("sitoAssociato", "il curatore ha già un sito associato");
+
+                    Intent intentDelegaSito = new Intent(getActivity(), DelegaSito.class);
+                    intentDelegaSito.putExtra("uid", utente.getUid()); //Optional parameters
+                    intentDelegaSito.putExtra("nome", utente.getNome()); //Optional parameters
+                    intentDelegaSito.putExtra("cognome", utente.getCognome()); //Optional parameters
+                    intentDelegaSito.putExtra("email", utente.getEmail()); //Optional parameters
+                    intentDelegaSito.putExtra("ruolo", utente.getRuolo()); //Optional parameters
+                    getActivity().startActivity(intentDelegaSito);
+                    }
 
 
+                }
+            });
 
-        elimina_sito.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intentEliminaSito = new Intent(getActivity(), EliminaSito.class) ;
-                intentEliminaSito.putExtra("uid", utente.getUid()); //Optional parameters
-                intentEliminaSito.putExtra("nome", utente.getNome()); //Optional parameters
-                intentEliminaSito.putExtra("cognome", utente.getCognome()); //Optional parameters
-                intentEliminaSito.putExtra("email", utente.getEmail()); //Optional parameters
-                intentEliminaSito.putExtra("ruolo", utente.getRuolo()); //Optional parameters
-                getActivity().startActivity(intentEliminaSito);
-            }
-        });
+
+            elimina_sito.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    checkSitoAssociatoAlCuratore(utente.getUid());
+                    Log.e("controlloSito", "Ho controllato se è associato il sito");
+
+                    if(sito == null){
+                        Toast.makeText(getActivity().getApplicationContext(), "Operazione non eseguibile: non è associato alcun sito", Toast.LENGTH_SHORT).show();
+                        Log.e("toat_creare", "dovrebbe essere visuaizzato il toast") ;
+                    }else {
+
+                        Intent intentEliminaSito = new Intent(getActivity(), EliminaSito.class);
+                        intentEliminaSito.putExtra("uid", utente.getUid()); //Optional parameters
+                        intentEliminaSito.putExtra("nome", utente.getNome()); //Optional parameters
+                        intentEliminaSito.putExtra("cognome", utente.getCognome()); //Optional parameters
+                        intentEliminaSito.putExtra("email", utente.getEmail()); //Optional parameters
+                        intentEliminaSito.putExtra("ruolo", utente.getRuolo()); //Optional parameters
+                        getActivity().startActivity(intentEliminaSito);
+                    }
+                }
+            });
+
 
 
 
@@ -98,4 +140,45 @@ public class FragmentImpostazioniSito extends Fragment {
         return utente;
     }
 
+
+
+    public void checkSitoAssociatoAlCuratore (String uidUtente){
+        Log.e("*****!!!!*****","controllo se c'è un sito associato al curatore!");
+        Query recentPostsQuery;
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://capitool-6a9ea-default-rtdb.europe-west1.firebasedatabase.app/");
+        myRef = database.getReference("/");
+
+        //-------------------------------------------------------------------------------------
+        // Ricerca per Nome
+
+        recentPostsQuery = myRef.child("Siti").orderByChild("uidCuratore").equalTo(uidUtente);     //SELECT * FROM Siti WHERE getUid LIKE "..."
+        recentPostsQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                ArrayList<SitoCulturale> siti = new ArrayList<>();
+                // Salva l'oggetto restituito in una lista di oggetti dello stesso tipo
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    Log.e("*****!!!!*****","SONO NEL FOR!!!");
+                    siti.add(snapshot.getValue(SitoCulturale.class));
+
+                }
+                if(siti.size() != 0) {
+                    Log.e("****SITI_0*****", siti.get(0).getNome());
+                    sito = siti.get(0);
+                }
+
+            }
+
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Getting Post failed, log a message
+                Log.w("QuertActivity", "loadPost:onCancelled", error.toException());
+            }
+        });
+
+
+    }
 }
