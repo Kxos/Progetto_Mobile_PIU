3b9c97bc858d658c3d1e9968ca95e345717d4d28
Andrea <andreax5000@gmail.com>
2022-02-01 15:29:48 +0100

    Aggiunto logo di avvio

---
+++ CapiTool/app/src/main/java/com/uniba/capitool/activities/StartLogo.java
@@ -0,0 +1,33 @@
+package com.uniba.capitool.activities;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.uniba.capitool.R;
+
+public class StartLogo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_logo);
+
+        //display the logo during 5 seconds,
+        new CountDownTimer(5000,1000){
+            @Override
+            public void onTick(long millisUntilFinished){}
+
+            @Override
+            public void onFinish(){
+                //set the new Content of your activity
+                Intent login = new Intent(StartLogo.this, Login.class);
+                Bundle transazione= ActivityOptions.makeSceneTransitionAnimation(StartLogo.this).toBundle();
+                startActivity(login, transazione);
+            }
+        }.start();
+    }
+}
