c043e78248805242cc63fbaa36f8cc52328a68c5
Andrea <andreax5000@gmail.com>
2022-02-01 15:54:00 +0100

    Ridotta durata logo all'avvio a 2.5 sec

--- CapiTool/app/src/main/java/com/uniba/capitool/activities/StartLogo.java
+++ CapiTool/app/src/main/java/com/uniba/capitool/activities/StartLogo.java
@@ -17,7 +17,7 @@ public class StartLogo extends AppCompatActivity {
         setContentView(R.layout.activity_start_logo);
 
         //display the logo during 5 seconds,
-        new CountDownTimer(5000,1000){
+        new CountDownTimer(2500,1000){
             @Override
             public void onTick(long millisUntilFinished){}
 
